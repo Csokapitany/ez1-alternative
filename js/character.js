950c747672df15a914cc7fc5363420b772dd0f40
Balazs Ferencz <ferenczbalazs85@gmail.com>
2023-03-24 08:32:07 +0000

    cleanup

--- js/character.js
+++ js/character.js
@@ -1,82 +1,34 @@
 
-//feltolto resz  Ez megy csak a szamokat stringbe viszi be
-let newCharacter = {
-    "charName": "",
-    "charCast": "",
-    "charLevel": 0,
-    "charKE": 0,
-    "charTE": 0,
-    "charVE": 0,
-    "charFP": 0,
-    "charEP": 0,
-    "charSFE": 0,
-    "charSPJ": 0,
-    "charSPB": 0,
-}
+// //feltolto resz  Ez megy csak a szamokat stringbe viszi be
+// let newCharacter = {
+//     "charName": "",
+//     "charCast": "",
+//     "charLevel": 0,
+//     "charKE": 0,
+//     "charTE": 0,
+//     "charVE": 0,
+//     "charFP": 0,
+//     "charEP": 0,
+//     "charSFE": 0,
+//     "charSPJ": 0,
+//     "charSPB": 0,
+// }
 
-function createKarakter(){
-    newCharacter.charName = document.getElementById("charName").value
-    newCharacter.charCast = document.getElementById("charCast").value
-    newCharacter.charLevel = document.getElementById("charLevel").value
-    newCharacter.charKE = document.getElementById("charKE").value
-    newCharacter.charTE = document.getElementById("charTE").value
-    newCharacter.charVE = document.getElementById("charVE").value
-    newCharacter.charFP = document.getElementById("charFP").value
-    newCharacter.charEP = document.getElementById("charEP").value
-    newCharacter.charSFE = document.getElementById("charSFE").value
-    newCharacter.charSPJ = document.getElementById("charSPJ").value
-    newCharacter.charSPB = document.getElementById("charSPB").value
+// function createKarakter(){
+//     newCharacter.charName = document.getElementById("charName").value
+//     newCharacter.charCast = document.getElementById("charCast").value
+//     newCharacter.charLevel = document.getElementById("charLevel").value
+//     newCharacter.charKE = document.getElementById("charKE").value
+//     newCharacter.charTE = document.getElementById("charTE").value
+//     newCharacter.charVE = document.getElementById("charVE").value
+//     newCharacter.charFP = document.getElementById("charFP").value
+//     newCharacter.charEP = document.getElementById("charEP").value
+//     newCharacter.charSFE = document.getElementById("charSFE").value
+//     newCharacter.charSPJ = document.getElementById("charSPJ").value
+//     newCharacter.charSPB = document.getElementById("charSPB").value
 
-    console.log(newCharacter)
-}
-
-// ez mar nem is kell 
- 
-
-// itt IIFE-t csinaltam a functionbol hogy ne kelljen a betolt gombbal hivni ennek erdemes utanannezni
-//onclick="dinamicNavigate(${karakter.id})"
-// (function createCard() {
-    // const xhr = new XMLHttpRequest();
-    // xhr.open("GET", `http://localhost:3000/karakter`, true);
-    
-    // xhr.onload = function() {
-    //     if(this.status === 200){
-    //         const karakter = JSON.parse(this.responseText);
-    //         let output = '';
-    //         karakter.forEach(function(karakter){ 
-    //         output += `
-    //                 <div class="col-md-6 col-lg-3">
-    //                     <div class="card bg-primary ">
-    //                         <div id="Load${karakter.id}"  class="btn-lg kiJelol">
-    //                             <div class="card-body text-center text-dark">
-    //                                 <li> Nev: ${karakter.charName}</li>
-    //                                 <li> Kaszt: ${karakter.charCast}</li>
-    //                                 <li> Szint: ${karakter.charLevel}</li>
-    //                                 <li> KE: ${karakter.charKE}</li>
-    //                                 <li> TE: ${karakter.charTE}</li>
-    //                                 <li> VE: ${karakter.charVE}</li>
-    //                                 <li> FP: ${karakter.charFP}</li>
-    //                                 <li> EP: ${karakter.charEP}</li>
-    //                                 <li> SFE: ${karakter.charSFE}</li>
-    //                                 <li> SPJ: ${karakter.charSPJ}</li>
-    //                                 <li> SPB: ${karakter.charSPB}</li>                                                               
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                 </div>    
-    //             `; 
-    //         });
-    //         document.getElementById("cardsPlace").innerHTML=output;
-    //     }
-    // }
-    // xhr.send();
-    
-
-
-
-
-
-
+//     console.log(newCharacter)
+// }
 
 
 
@@ -91,7 +43,7 @@ function createKarakter(){
                 let div = document.createElement('div');
     
                 div.innerHTML = (`     
-                    <div class="card text-center text-dark p-3 d-flex justify-content-center kiJelol" onclick="valami(${element.id})" id="${element.id}" ">
+                    <div class="card text-center text-dark p-3 d-flex justify-content-center kiJelol" onclick="select(${element.id})" id="${element.id}" ">
                         <div class="m-2">                                           
                             <img class="avatar" src="${element.image}">
                         </div>  
@@ -120,7 +72,7 @@ function createKarakter(){
 
 var cardsToFight = [];
 
-function valami(id){
+function select(id){
     console.log(id);
     
     var card = document.getElementById(`${id}`);
